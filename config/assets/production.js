19a698e51c7c749c2a7d9a7fe0c75f77b91617b0
Cephas Lin <cephalin@microsoft.com>
2017-08-24 17:06:21 +0200

    test

--- config/assets/production.js
+++ config/assets/production.js
@@ -7,23 +7,31 @@ module.exports = {
     lib: {
       css: [
         // bower:css
-        'public/lib/angular-ui-notification/dist/angular-ui-notification.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/bootstrap/dist/css/bootstrap.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/angular-ui-notification/dist/angular-ui-notification.css',
+        'public/lib/font-awesome/css/font-awesome.min.css',
+        'public/lib/moment-picker/dist/angular-moment-picker.min.css'
         // endbower
       ],
       js: [
         // bower:js
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-messages/angular-messages.min.js',
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/bootstrap/dist/js/bootstrap.min.js',
+        'public/lib/angular/angular.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-i18n/angular-locale_de-de.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/ng-file-upload/ng-file-upload.js',
+        'public/lib/angular-messages/angular-messages.js',
         'public/lib/angular-mocks/angular-mocks.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-ui-notification/dist/angular-ui-notification.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/ng-file-upload/ng-file-upload.min.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-ui-notification/dist/angular-ui-notification.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/tether/dist/js/tether.min.js',
+        'public/lib/moment/min/moment-with-locales.min.js',
+        'public/lib/moment-picker/dist/angular-moment-picker.min.js'
         // endbower
       ]
     },
